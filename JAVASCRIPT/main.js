b2c62037d4c7cf155bc1502e6daeb9c676c3156e
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-14 11:48:37 -0400

    embed instead of Spotify aAPI, dont want uiser to haver to log in

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,6 +1,6 @@
-const APIController = (function () {
-  const clientId = "CLIENT ID";
-  const clientSecret = "CLIENT SECRET";
+/*const APIController = (function () {
+  const clientId = "d5cb1a5cfd304e1daad3362fb3b231e5";
+  const clientSecret = "92090381e11845c2ab551eb6fac53368";
 
   const _getToken = async () => {
     const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -16,8 +16,10 @@ const APIController = (function () {
     return data.access_token;
   };
 
-  const _getTrack = async (token, trackEndPoint) => {
-    const result = await fetch(`${trackEndPoint}`, {
+  const _getTrack = async (BQD7XEZQkBWyIldQrfTaU8onsJQZO-4fo5dSRGg8CrnNNfGgoBd_Mt_1P4E3kojDCM8U4VEvjd-pDHqR8imrXeee74fHmP78IXTQXWQO5mhUggfRa7o9aFroMvHWKJeeSy-eTLa617f_y3E, https://api.spotify.com/v1/tracks/{id}
+) => {
+    const result = await fetch(`${https://api.spotify.com/v1/tracks/{id}
+}`, {
       method: "GET",
       headers: { Authorization: "Bearer" + token },
     });
@@ -27,8 +29,9 @@ const APIController = (function () {
   };
 
   return {
-    getTrack(token, trackEndPoint) {
-      return _getTrack(token, trackEndPoint);
+    getTrack(BQD7XEZQkBWyIldQrfTaU8onsJQZO-4fo5dSRGg8CrnNNfGgoBd_Mt_1P4E3kojDCM8U4VEvjd-pDHqR8imrXeee74fHmP78IXTQXWQO5mhUggfRa7o9aFroMvHWKJeeSy-eTLa617f_y3E, https://api.spotify.com/v1/tracks/{id}
+) {
+      return _getTrack(BQD7XEZQkBWyIldQrfTaU8onsJQZO-4fo5dSRGg8CrnNNfGgoBd_Mt_1P4E3kojDCM8U4VEvjd-pDHqR8imrXeee74fHmP78IXTQXWQO5mhUggfRa7o9aFroMvHWKJeeSy-eTLa617f_y3E, trackEndPoint);
     },
   };
-})();
+})(); */
