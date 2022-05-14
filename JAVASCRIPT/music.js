c92e6f947a47bc10f9e93821a45c31348d9335bd
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-14 14:35:33 -0400

    all artist VS selected, hover functions on spotify button

--- JAVASCRIPT/music.js
+++ JAVASCRIPT/music.js
@@ -1,5 +1,5 @@
 import SpotifyWebApi from "spotify-web-api-node";
-
+//import SpotifyWebApi from "spotify-web-api-js";
 const clientId = "d5cb1a5cfd304e1daad3362fb3b231e5";
 const clientSecret = "92090381e11845c2ab551eb6fac53368";
 const redirectUri = "http://localhost:3000/play";
