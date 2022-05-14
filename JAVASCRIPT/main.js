b0c7d7d56bbaf9cb062f4a31d892b5caf8309940
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-14 10:32:01 -0400

    added checkboxes, centered img

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,35 +1,34 @@
-const APIController = (function() {
-
+const APIController = (function () {
   const clientId = "CLIENT ID";
   const clientSecret = "CLIENT SECRET";
 
   const _getToken = async () => {
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic" + btoa(clientId + ":" + clientSecret),
+      },
+      body: "grant_type=client_credentials",
+    });
 
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/x-www-form-urlencoded",
-            "Authorization" : "Basic" + btoa(clientId + ":" + clientSecret)
-        },
-        body: "grant_type=client_credentials"
-        });
+    const data = await result.json();
+    return data.access_token;
+  };
 
-        const data = await result.json();
-        return data.access_token;
-    }
+  const _getTrack = async (token, trackEndPoint) => {
+    const result = await fetch(`${trackEndPoint}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer" + token },
+    });
 
-    const _getTrack = async (token, trackEndPoint) => {
+    const data = await result.json();
+    return data;
+  };
 
-        const result = await fetch(`${trackEndPoint}`, {
-            method: "GET",
-            headers: { "Authorization" : "Bearer" + token}
-        });
-
-        const data = await result.json();
-        return data;
-    }
-
+  return {
     getTrack(token, trackEndPoint) {
-        return _getTrack(token, trackEndPoint)
-    }
-  })(); 
+      return _getTrack(token, trackEndPoint);
+    },
+  };
+})();
