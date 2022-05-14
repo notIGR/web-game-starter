98d1fbf16b855adcaaf4ba1094d940447d98fb2b
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-14 12:18:08 -0400

    SPOTIFY API FUNTIONAL

---
+++ JAVASCRIPT/music.js
@@ -0,0 +1,9 @@
+
+import SpotifyWebApi from "spotify-web-api-node";
+const clientId = "d5cb1a5cfd304e1daad3362fb3b231e5";
+const clientSecret = "92090381e11845c2ab551eb6fac53368";
+
+const spotifyApi = new SpotifyWebApi({
+	clientId,
+	clientSecret
+});
