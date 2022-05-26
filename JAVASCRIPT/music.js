bcbeb060e670d7c503dddba444ea5a500f55471c
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-25 21:40:38 -0400

    buttons are working :)

--- JAVASCRIPT/music.js
+++ JAVASCRIPT/music.js
@@ -1,9 +1,17 @@
-const btnShow= document.querySelector("button");
-const result = document.getElementById("c1");
-
-
-btnShow.addEventListener("click", (e) => {
-	const selected = document.querySelector("input[type=\"radio\"]:checked");
-	result.innerText = selected.value;
-	e.preventDefault();
-});
+for (let i = 0; i < 5; i++) {
+	const btnShow = document.querySelector(`#b${i + 1}`);
+	const result = document.querySelector(`#c${i + 1}`);
+	btnShow.addEventListener("click", (e) => {
+		e.preventDefault();
+		const group = document.querySelector(`#group${i + 1}`);
+		let selected;
+		if(group.children[0].checked){
+			selected =group.children[0];
+		}
+		else{
+			selected = group.children[4];
+		}
+		
+		result.innerText = selected.value;
+	});
+}
